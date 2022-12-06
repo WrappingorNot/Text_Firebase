c29d2c93ec2eada450008c4109b69f5e85868244
WrappingorNot <ksy4717@gmail.com>
2022-12-06 16:58:29 +0900

    android firebase create

--- app/src/main/java/com/example/text_firebase/MainActivity.kt
+++ app/src/main/java/com/example/text_firebase/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.text_firebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var  auth: FirebaseAuth
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        auth = FirebaseAuth.getInstance()
+
+        auth.createUserWithEmailAndPassword("abc@naver.com", "123123")
+            .addOnCompleteListener(this){ task ->
+                if(task.isSuccessful){
+
+                    Toast.makeText(baseContext, "Authenticaion Success", Toast.LENGTH_LONG).show()
+                }else{
+
+                    Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_LONG).show()
+                }
+
+            }
+
+
     }
 }
