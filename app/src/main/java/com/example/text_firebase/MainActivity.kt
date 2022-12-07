f85fef1dec516eae5027bc543ebf688de385f6d2
WrappingorNot <ksy4717@gmail.com>
2022-12-07 10:31:10 +0900

    firebase login session create

--- app/src/main/java/com/example/text_firebase/MainActivity.kt
+++ app/src/main/java/com/example/text_firebase/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.text_firebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 
@@ -13,18 +15,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var joinButton = findViewById<Button>(R.id.joinButton)
+        var email_form = findViewById<EditText>(R.id.email_form)
+        var pass_form = findViewById<EditText>(R.id.password_form)
 
         auth = FirebaseAuth.getInstance()
 
-        auth.createUserWithEmailAndPassword("abc@naver.com", "123123")
-            .addOnCompleteListener(this){ task ->
-                if(task.isSuccessful){
+        joinButton.setOnClickListener(){
+
+        auth.createUserWithEmailAndPassword(email_form.text.toString(), pass_form.text.toString())
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
 
                     Toast.makeText(baseContext, "Authenticaion Success", Toast.LENGTH_LONG).show()
-                }else{
+                } else {
 
                     Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_LONG).show()
                 }
+            }
 
             }
 
