62efb141b6d3a9f9fb848aeadf66ea06803f1b8f
WrappingorNot <ksy4717@gmail.com>
2022-12-08 13:38:31 +0900

    firebase realtime database List create

--- app/src/main/java/com/example/text_firebase/BoardInputActivity.kt
+++ app/src/main/java/com/example/text_firebase/BoardInputActivity.kt
@@ -2,13 +2,20 @@ package com.example.text_firebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
 
 class BoardInputActivity : AppCompatActivity() {
 
     private val TAG = BoardInputActivity::class.java.simpleName
 
-    private var uid:String? = null
+    private var uid:String? = ""
+    var form_button = findViewById<Button>(R.id.form_button)
+    var text_form = findViewById<EditText>(R.id.text_form)
+    var context_text_form = findViewById<EditText>(R.id.context_text_form)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +24,25 @@ class BoardInputActivity : AppCompatActivity() {
         if (intent.hasExtra(("uid"))){
 
             uid = intent.getStringExtra("uid")
+
+        }
+
+
+
+        form_button.setOnClickListener{
+
+            val database = FirebaseDatabase.getInstance()
+            val myRef = database.getReference()
+
+
+            val dataInput = DataModel(
+
+                text_form.text.toString(),
+                context_text_form.text.toString()
+
+            )
+
+            myRef.child(uid.toString()).setValue(dataInput)
         }
 
 
