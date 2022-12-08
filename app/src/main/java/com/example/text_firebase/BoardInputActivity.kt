a738f30e65901d1c59cf32c76673e2f0c8a0574f
WrappingorNot <ksy4717@gmail.com>
2022-12-08 10:13:44 +0900

    firebase realtime database List create

--- app/src/main/java/com/example/text_firebase/BoardInputActivity.kt
+++ app/src/main/java/com/example/text_firebase/BoardInputActivity.kt
@@ -5,14 +5,11 @@ import android.os.Bundle
 import com.google.firebase.auth.FirebaseAuth
 
 class BoardInputActivity : AppCompatActivity() {
-    private val TAG = BoardListActivity::class.java.simpleName
 
-    private lateinit var auth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_board_input)
 
-        auth = FirebaseAuth.getInstance()
 
     }
 }
--- app/src/main/java/com/example/text_firebase/BoardListActivity.kt
+++ app/src/main/java/com/example/text_firebase/BoardListActivity.kt
@@ -6,12 +6,15 @@ import com.google.firebase.auth.FirebaseAuth
 
 class BoardListActivity : AppCompatActivity() {
 
+    private val TAG = BoardListActivity::class.java.simpleName
 
+    private lateinit var auth : FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_board_list)
 
+        auth = FirebaseAuth.getInstance()
 
 
     }
