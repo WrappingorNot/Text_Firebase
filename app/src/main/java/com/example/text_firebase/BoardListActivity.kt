be071a6167d274076e4d47b7ce80a34066447c8d
WrappingorNot <ksy4717@gmail.com>
2022-12-09 13:02:00 +0900

    firebase realtime database List create

--- app/src/main/java/com/example/text_firebase/BoardListActivity.kt
+++ app/src/main/java/com/example/text_firebase/BoardListActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.example.text_firebase.databinding.ActivityBoardListBinding
 import com.google.firebase.auth.FirebaseAuth
 
 class BoardListActivity : AppCompatActivity() {
@@ -13,23 +14,25 @@ class BoardListActivity : AppCompatActivity() {
     private lateinit var auth : FirebaseAuth
 
 
+    private lateinit var binding: ActivityBoardListBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_board_list)
-
+        binding = ActivityBoardListBinding.inflate(layoutInflater)
+        val view = binding.root
         auth = FirebaseAuth.getInstance()
 
+        setContentView(view)
 
 
-//        write_button.setOnClickListener{
-//
-//
-//            val intent = Intent(this, BoardInputActivity::class.java)
-//            intent.putExtra("uid", auth.currentUser?.uid)
-//            startActivity(intent)
-//
-//        }
+
+
+
+        val intent = Intent(this, BoardInputActivity::class.java)
+        intent.putExtra("uid", auth.currentUser?.uid)
+        startActivity(intent)
+
 
 
     }
