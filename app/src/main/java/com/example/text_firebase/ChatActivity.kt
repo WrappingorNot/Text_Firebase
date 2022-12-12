c6c373b9c4f83a923310a33b91b2af20adff5d15
WrappingorNot <ksy4717@gmail.com>
2022-12-12 09:06:15 +0900

    firebase realtime database List create

--- app/src/main/java/com/example/text_firebase/ChatActivity.kt
+++ app/src/main/java/com/example/text_firebase/ChatActivity.kt
@@ -3,15 +3,20 @@ package com.example.text_firebase
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
 
 class ChatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
 
-
+        var chat_view = findViewById<EditText>(R.id.user_chat)
+        var user_edit = findViewById<EditText>(R.id.user_edit)
+        var user_next = findViewById<Button>(R.id.user_next)
+        var chat_list = findViewById<ListView>(R.id.chat_list)
         var intetent =  Intent(this, MainActivity::class.java)
-        startActivity(intent)
 
     }
 }
